06b180748a20c6bdd7adb5ec1f36b64344f0aee1
chorogchip <chorogchip@naver.com>
2026-04-30 22:41:51 +0900

    create process and context switch

--- kernel.c
+++ kernel.c
@@ -26,7 +26,8 @@ __attribute__((naked))
 __attribute__((aligned(4)))
 void kernel_entry(void) {
     __asm__ __volatile__(
-        "csrw sscratch, sp\n"
+
+        "csrrw sp, sscratch, sp\n"
         "addi sp, sp, -4 * 31\n"
         "sw ra, 4 * 0(sp)\n"
         "sw gp, 4 * 1(sp)\n"
@@ -61,6 +62,9 @@ void kernel_entry(void) {
         
         "csrr a0, sscratch\n"
         "sw a0, 4 * 30(sp)\n"
+
+        "addi a0, sp, 4 * 31\n"
+        "csrw sscratch, a0\n"
 
         "mv a0, sp\n"
         "call handle_trap\n"
@@ -121,21 +125,159 @@ paddr_t alloc_pages(uint32_t n) {
     return paddr;
 }
 
-
-
 void putchar(char ch) {
     sbi_call(ch, 0, 0, 0, 0, 0, 0, 1);
 }
 
+#define PROCS_MAX 8
+#define PROC_UNUSED 0
+#define PROC_RUNNABLE 1
+
+struct process {
+    int pid;
+    int state;
+    vaddr_t sp;
+    uint8_t stack[8192];
+};
+
+struct process procs[PROCS_MAX];
+struct process *current_proc;
+struct process *idle_proc;
+
+__attribute__((naked)) void switch_context(uint32_t *prev_sp, uint32_t *next_sp) {
+    __asm__ __volatile__(
+        
+        "addi sp, sp, -13 * 4\n"
+        "sw ra, 0 * 4(sp)\n"
+        "sw s0, 1 * 4(sp)\n"
+        "sw s1, 2 * 4(sp)\n"
+        "sw s2, 3 * 4(sp)\n"
+        "sw s3, 4 * 4(sp)\n"
+        "sw s4, 5 * 4(sp)\n"
+        "sw s5, 6 * 4(sp)\n"
+        "sw s6, 7 * 4(sp)\n"
+        "sw s7, 8 * 4(sp)\n"
+        "sw s8, 9 * 4(sp)\n"
+        "sw s9, 10 * 4(sp)\n"
+        "sw s10, 11 * 4(sp)\n"
+        "sw s11, 12 * 4(sp)\n"
+
+        "sw sp, (a0)\n"
+        "lw sp, (a1)\n"
+
+        "lw ra, 0 * 4(sp)\n"
+        "lw s0, 1 * 4(sp)\n"
+        "lw s1, 2 * 4(sp)\n"
+        "lw s2, 3 * 4(sp)\n"
+        "lw s3, 4 * 4(sp)\n"
+        "lw s4, 5 * 4(sp)\n"
+        "lw s5, 6 * 4(sp)\n"
+        "lw s6, 7 * 4(sp)\n"
+        "lw s7, 8 * 4(sp)\n"
+        "lw s8, 9 * 4(sp)\n"
+        "lw s9, 10 * 4(sp)\n"
+        "lw s10, 11 * 4(sp)\n"
+        "lw s11, 12 * 4(sp)\n"
+        "addi sp, sp, 13 * 4\n"
+        "ret\n"
+    );
+}
+
+struct process *create_process(uint32_t pc) {
+    struct process *proc = NULL;
+    int i;
+    for (i = 0; i < PROCS_MAX; ++i) {
+        if (procs[i].state == PROC_UNUSED) {
+            proc = &procs[i];
+            break;
+        }
+    }
+
+    if (!proc)
+        PANIC("on free process slots");
+
+    uint32_t *sp = (uint32_t*) &proc->stack[sizeof(proc->stack)];
+    *--sp = 0; // s11
+    *--sp = 0; // s10
+    *--sp = 0; // s9
+    *--sp = 0; // s8
+    *--sp = 0; // s7
+    *--sp = 0; // s6
+    *--sp = 0; // s5
+    *--sp = 0; // s4
+    *--sp = 0; // s3
+    *--sp = 0; // s2
+    *--sp = 0; // s1
+    *--sp = 0; // s0
+    *--sp = (uint32_t) pc; // ra
+                
+    proc->pid = i + 1;
+    proc->state = PROC_RUNNABLE;
+    proc->sp = (uint32_t) sp;
+    return proc;
+}
+
+void yield(void) {
+
+    struct process *next = idle_proc;
+
+    __asm__ __volatile__(
+        "csrw sscratch, %[sscratch]\n"
+        :
+        : [sscratch] "r" ((uint32_t)&next->stack[sizeof(next->stack)])
+    );
+
+    struct process *prev = current_proc;
+    current_proc = next;
+    switch_context(&prev->sp, &next->sp);
+}
+
+void delay(void) {
+    for (int i = 0; i < 30000000; i++)
+        __asm__ __volatile__("nop");
+}
+
+struct process *proc_a, *proc_b;
+
+void proc_a_entry(void) {
+    printf("starting process A\n");
+    while (1) {
+        putchar('A');
+        yield();
+    }
+}
+
+void proc_b_entry(void) {
+    printf("starting process B\n");
+    while (1) {
+        putchar('B');
+        yield();
+    }
+}
+
+
 void kernel_main(void) {
     memset(__bss, 0, (size_t) __bss_end - (size_t)__bss);
 
-    paddr_t paddr0 = alloc_pages(2);
-    paddr_t paddr1 = alloc_pages(1);
-    printf("alloc pages test: paddr0=%x\n", paddr0);
-    printf("alloc pages test: paddr1=%x\n", paddr1);
+    printf("\n\n");
 
-    PANIC("booted!");
+    WRITE_CSR(stvec, (uint32_t) kernel_entry);
+
+    idle_proc = create_process((uint32_t) NULL);
+    idle_proc->pid = 0;
+    current_proc = idle_proc;;
+
+    proc_a = create_process((uint32_t) proc_a_entry);
+    proc_b = create_process((uint32_t) proc_b_entry);
+    
+    yield();
+    PANIC("switched to idle process");
+    
+    //paddr_t paddr0 = alloc_pages(2);
+    //paddr_t paddr1 = alloc_pages(1);
+    //printf("alloc pages test: paddr0=%x\n", paddr0);
+    //printf("alloc pages test: paddr1=%x\n", paddr1);
+
 
 }
 
